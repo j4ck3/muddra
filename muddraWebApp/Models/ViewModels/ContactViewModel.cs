6adaceec43252e76a6a29d06fb4153f5e5b50676
j4ck3 <jacobhallgren@live.se>
2023-09-10 04:04:28 +0200

    contact form

---
+++ muddraWebApp/Models/ViewModels/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace muddraWebApp.Models.ViewModels;
+
+public class ContactViewModel
+{
+    [Required(ErrorMessage = "*")]
+    [Display(Name = "E-post")]
+    [EmailAddress(ErrorMessage = "Ange en giltig E-post adress")]
+    public string Email { get; set; } = null!;
+
+    [MinLength(20, ErrorMessage = "Medelandet måste vara minst 20 bokstäver långt")]
+    [Display(Name = "Medelande")]
+    [Required(ErrorMessage = "*")]
+    public string Message { get; set; } = null!;
+
+    [Display(Name = "Adress / Område")]
+    [Required(ErrorMessage = "*")]
+    public string Area { get; set; } = null!;
+}
---
+++ muddraWebApp/Services/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace muddraWebApp.Services;
+
+public class EmailService
+{
+    public async Task SendEmailAsync(string email, string message)
+    {
+       var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        try
+        {
+            var smtpSettingsSection = configuration.GetSection("SmtpSettings");
+            var host = smtpSettingsSection.GetValue<string>("Host");
+            var port = smtpSettingsSection.GetValue<int>("Port");
+            var userName = smtpSettingsSection.GetValue<string>("RelayUsername");
+            var key = smtpSettingsSection.GetValue<string>("RelayKey");
+            var reciver = "contact@muddra.com";
+            var sender = "noreply@muddra.com";
+
+
+            using var client = new SmtpClient(host, port);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(userName, key);
+
+            var subject = "Nytt meddelande från " + email;
+
+            var mailMessage = new MailMessage(from: sender, to: reciver, subject, message);
+
+            await client.SendMailAsync(mailMessage);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error sending email: {ex.Message}");
+            throw; 
+        }
+    }
+}
